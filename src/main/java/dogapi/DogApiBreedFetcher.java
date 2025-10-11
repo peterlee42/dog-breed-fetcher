cc379d4f9078a15c899faa14cfcba7f4b754e9f2
peterlee42 <76502486+peterlee42@users.noreply.github.com>
2025-10-11 14:33:59 -0400

    completed DogApiBreedFetcher

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -4,6 +4,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -16,20 +17,47 @@ import java.util.*;
  */
 public class DogApiBreedFetcher implements BreedFetcher {
     private final OkHttpClient client = new OkHttpClient();
+    private static final String API_URL_STRING = "https://dog.ceo/api/";
+    private static final String STATUS = "status";
+    private static final String STATUS_SUCCESS = "success";
+    private static final String MESSAGE = "message";
 
     /**
      * Fetch the list of sub breeds for the given breed from the dog.ceo API.
+     * 
      * @param breed the breed to fetch sub breeds for
      * @return list of sub breeds for the given breed
-     * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
+     * @throws BreedNotFoundException if the breed does not exist (or if the API
+     *                                call fails for any reason)
      */
     @Override
     public List<String> getSubBreeds(String breed) {
-        // TODO Task 1: Complete this method based on its provided documentation
-        //      and the documentation for the dog.ceo API. You may find it helpful
-        //      to refer to the examples of using OkHttpClient from the last lab,
-        //      as well as the code for parsing JSON responses.
+        // Task 1: Complete this method based on its provided documentation
+        // and the documentation for the dog.ceo API. You may find it helpful
+        // to refer to the examples of using OkHttpClient from the last lab,
+        // as well as the code for parsing JSON responses.
         // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+
+        final Request request = new Request.Builder().url(String.format("%sbreed/%s/list", API_URL_STRING, breed))
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            final JSONObject responseBody = new JSONObject(response.body().string());
+
+            if (!responseBody.getString(STATUS).equals(STATUS_SUCCESS)) {
+                throw new BreedNotFoundException(breed);
+            }
+
+            JSONArray subBreeds = responseBody.getJSONArray(MESSAGE);
+            ArrayList<String> subBreedsArray = new ArrayList<>();
+            for (int i = 0; i < subBreeds.length(); i++) {
+                subBreedsArray.add(subBreeds.get(i).toString());
+            }
+
+            return subBreedsArray;
+
+        } catch (IOException | JSONException event) {
+            throw new BreedNotFoundException(breed);
+        }
     }
 }
