08dc3b847501b62c3cfea95f152c834160d296b4
peterlee42 <76502486+peterlee42@users.noreply.github.com>
2025-10-11 16:12:33 -0400

    changed BreedNotFoundException to be a Checked Exception

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -27,7 +27,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
      *                                call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
         // Task 1: Complete this method based on its provided documentation
         // and the documentation for the dog.ceo API. You may find it helpful
         // to refer to the examples of using OkHttpClient from the last lab,
